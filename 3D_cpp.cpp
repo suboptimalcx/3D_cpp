5adfef508f7b1d3386752079d51463e8f2104477
vikaruscxx <wixar2005@gmail.com>
2025-02-20 18:56:40 +0100

    initial commit

---
+++ 3D_cpp.cpp
@@ -0,0 +1,12 @@
+﻿// 3D_cpp.cpp : Defines the entry point for the application.
+//
+
+#include "3D_cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello and have fun." << endl;
+	return 0;
+}
